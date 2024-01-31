b0652ec8adaa543c3c97dbc6b642f792e1be409a
stelladimo <stella.demopoulou@gmail.com>
2024-01-31 19:40:55 +0200

    resource

--- src/main/java/com/example/weleadfoodapp/transfer/resource/StoreResource.java
+++ src/main/java/com/example/weleadfoodapp/transfer/resource/StoreResource.java
@@ -1,20 +1,15 @@
 package com.example.weleadfoodapp.transfer.resource;
 
-import com.example.weleadfoodapp.model.StoreCategory;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Setter
 @ToString(callSuper = true)
 public class StoreResource extends BaseResource {
     private String name;
-    private StoreCategory storeCategory;
-    private Set<StoreResource> stores = new HashSet<>();
+    private StoreCategoryResource storeCategory;
 
 
 }
